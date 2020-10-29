a5d4ae950dae1b074e665c40b6b2775423c3d189
Irina Kramer <irakramer@gmail.com>
2020-10-28 20:45:45 -0700

    added css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
